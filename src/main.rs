e91cb8568fa138e3b9dbb4b6836661f5edac3ce8
wanjikuKim-collab <wanjikukimani24@gmail.com>
2023-11-29 16:56:28 +0300

    renamed the clap::Command import to ClapCommand

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate clap;
-use clap::{Arg, Command};
-use std::process::{exit};
+use clap::{Arg, Command as ClapCommand};
+use std::process::{Command, exit};
 
 fn main() {
     // Create the App builder
-    let app = Command::new("Movie Packager")
+    let app = ClapCommand::new("Movie Packager")
         .version("1.0")
         .author("Faith Kimani")
         .about("Packaging movies using ffmpeg")
@@ -31,28 +31,12 @@ fn main() {
 
     // Extract values of parsed arguments
     let input_files:&String = matches.get_one("input").unwrap();
-    let output_file:&Str = matches.get_one("output").unwrap();
+    let output_file:&String = matches.get_one("output").unwrap();
 
-    // Convert input file string to vector of input files
-    let input_files_vec: Vec<&str> = input_files.split(',').collect();
+    //Converting input file string to vector of input files
+    let input_files: Vec<&str> = input_files.split(',').collect();
 
-    // Simplify FFMPEG Command Creation
-    let input_files_str = input_files_vec.join(" -i ");
-    let ffmpeg_command = format!(
-        "ffmpeg -i {} -filter_complex concat=n={}:v=1:a=1 -y {}",
-        input_files_str,
-        input_files_vec.len(),
-        output_file
-    );
+    
 
-    // Execute ffmpeg command using std::process::Command
-    let status = Command::new("sh").arg("-c").arg(&ffmpeg_command).status().unwrap();
-
-    // Handle FFMPEG Exit Status
-    if status.success() {
-        println!("Movies packaged successfully into: {}", output_file);
-    } else {
-        eprintln!("Error: Failed to create the package.");
-        exit(1);
-    }
 }
+
